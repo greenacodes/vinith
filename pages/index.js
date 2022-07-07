2bf4bb688d841c9d5d0dcb3e8f9dd43d3e8d4046
greenacodes <83861368+greenacodes@users.noreply.github.com>
2022-07-08 00:45:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          Hello
+
+       </p>
+          <marquee><b>vinith</b></marquee>
+
       </main>
 
       <Footer />
